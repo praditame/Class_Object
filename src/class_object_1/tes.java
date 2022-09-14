4bbc60ec3f8909fdffffa9908b21522a3a6827d4
M306 <M306@M307>
2022-09-14 09:01:34 +0700

    upload new project

---
+++ src/class_object_1/tes.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class_object_1;
+
+/**
+ *
+ * @author M306
+ */
+public class tes {
+    
+}
